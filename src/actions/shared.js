35facc98483ee25fd5fa37a920e14482db241ee8
aehazard <32916110+aehazard@users.noreply.github.com>
2021-12-12 14:44:31 -0600

    Dashboard connected to store

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -8,10 +8,10 @@ const AUTHED_ID = 'tylermcginnis'
 export function handleInitialData () {
   return (dispatch) => {
     return getInitialData()
-      .then(({ users, tweets }) => {
+      .then(({ users, tweets, id }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
-        dispatch(setAuthUser(id))
+        dispatch(setAuthUser(AUTHED_ID))
       })
   }
 }
