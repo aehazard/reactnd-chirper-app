3265fddae6f14d0b513ed1a661af5ef0ea2f5912
aehazard <32916110+aehazard@users.noreply.github.com>
2021-12-12 18:39:00 -0600

    Loading bar added

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
 import { setAuthUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
-      .then(({ users, tweets, id }) => {
+      .then(({ users, tweets}) => {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
         dispatch(setAuthUser(AUTHED_ID))
+        dispatch(hideLoading())
       })
   }
 }
