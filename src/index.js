150c49517920ec3568ab01f98e8e01bcf6e3dbed
aehazard <32916110+aehazard@users.noreply.github.com>
2021-12-14 13:33:07 -0600

    Routing added

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
-import BrowserRouter from 'react-router-dom'
 
 const store = createStore(reducer, middleware)
 
 ReactDOM.render(
   <Provider store ={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>,
 document.getElementById('root'))
